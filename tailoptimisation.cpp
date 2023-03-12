3ed08880389105bf86b2d429980d1aca5e3ac372
Riyanshu Dubey <76859260+printoffriyanshu@users.noreply.github.com>
2023-03-12 22:57:15 +0530

    Create tailoptimisation.cpp

---
+++ tailoptimisation.cpp
@@ -0,0 +1,22 @@
+void quickSort(int arr[], int low, int high)
+{
+  while (low < high)
+  {
+    
+    int pi = partition(arr, low, high);
+ 
+
+    if (pi - low < high - pi)
+    {
+      quickSort(arr, low, pi - 1);
+      low = pi + 1;
+    }
+ 
+   
+    else
+    {
+      quickSort(arr, pi + 1, high);
+      high = pi - 1;
+    }
+  }
+}
